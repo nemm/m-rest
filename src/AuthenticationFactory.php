3438cab3beb3bd645363a3280a3f778bf3e2763a
Adam Samborowski <nemo@zmap.pl>
2022-08-14 13:58:06 +0000

    Some cleaning

--- src/AuthenticationFactory.php
+++ src/AuthenticationFactory.php
@@ -1,30 +1,28 @@
 <?php
 
-# Savepoint
+    namespace Nemm\MRest;
 
-namespace Nemm\MRest;
+    /**
+     * Factory for creating Authentication
+     */
+    class AuthenticationFactory {
 
-/**
- * Factory for creating Authentication
- */
-class AuthenticationFactory {
-    
-    public static function createNoAuth(){
-        return new Authentication();
+        public static function createNoAuth(){
+            return new Authentication();
+        }
+
+        public static function createBasicAuth( string $username = '', string $password = ''){
+            return 
+                (new Authentication())
+                    ->setType(AuthenticationType::BASIC)
+                    ->setUsername($username)
+                    ->setPassword($password);
+        }
+
+        public static function createJWTAuth( string $token = ''){
+            return 
+                (new Authentication())
+                    ->setType(AuthenticationType::JWT)
+                    ->setToken($token);
+        }
     }
-    
-    public static function createBasicAuth( string $username = '', string $password = ''){
-        return 
-            (new Authentication())
-                ->setType(AuthenticationType::BASIC)
-                ->setUsername($username)
-                ->setPassword($password);
-    }
-    
-    public static function createJWTAuth( string $token = ''){
-        return 
-            (new Authentication())
-                ->setType(AuthenticationType::JWT)
-                ->setToken($token);
-    }
-}
--- src/ResponseData.php
+++ src/ResponseData.php
@@ -1,34 +1,31 @@
 <?php
+    namespace Nemm\MRest;
 
-# Savepoint
+    /**
+     * Class for keeping nicely formatted response
+     */
+    class ResponseData {
+        private int $code;
+        private ?array $data;
 
-namespace Nemm\MRest;
+        public function __construct(int $code, ?array $data) {
+            $this->code = $code;
+            $this->data = $data;
+        }
 
-/**
- * Class for keeping nicely formatted response
- */
-class ResponseData {
-    private int $code;
-    private ?array $data;
-    
-    public function __construct(int $code, ?array $data) {
-        $this->code = $code;
-        $this->data = $data;
+        public function getCode(): int {
+            return $this->code;
+        }
+
+        public function getData(): ?array {
+            return $this->data;
+        }
+
+        public function setCode(int $code): void {
+            $this->code = $code;
+        }
+
+        public function setData(?array $data): void {
+            $this->data = $data;
+        }
     }
-    
-    public function getCode(): int {
-        return $this->code;
-    }
-
-    public function getData(): ?array {
-        return $this->data;
-    }
-
-    public function setCode(int $code): void {
-        $this->code = $code;
-    }
-
-    public function setData(?array $data): void {
-        $this->data = $data;
-    }
-}
